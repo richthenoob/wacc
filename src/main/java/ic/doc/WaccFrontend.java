62671b62d698cd836b62d762db28e9fbed5d11e9
Jia Qi Poon <jqp18@ic.ac.uk>
2021-02-03 00:19:40 +0800

    [jqp18] Update frontend to read tests from files

--- src/main/java/ic/doc/WaccFrontend.java
+++ src/main/java/ic/doc/WaccFrontend.java
@@ -1,14 +1,18 @@
 package ic.doc;
 
 import ic.doc.antlr.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 public class WaccFrontend {
 
-    public static String parse(String stringInput) {
+    public static String parse(InputStream inputStream) throws IOException {
         // create a CharStream that reads from standard input
-        CharStream input = CharStreams.fromString(stringInput);
+        CharStream input = CharStreams.fromStream(inputStream);
 
         // create a lexer that feeds off of input CharStream
         BasicLexer lexer = new BasicLexer(input);
@@ -24,7 +28,19 @@ public class WaccFrontend {
         return tree.toStringTree(parser);
     }
 
-    public static void main(String[] args) {
-        System.out.println(parse(args[0]));
+    public static void main(String[] args) throws IOException {
+
+        if (args.length < 1) {
+            System.out.println("Please provide a filepath.");
+            return;
+        }
+
+        File file = new File(args[0]);
+        if (file.exists()) {
+            InputStream inputStream = new FileInputStream(file);
+            System.out.println(parse(inputStream));
+        } else {
+            System.out.println("Invalid filepath provided: " + args[0]);
+        }
     }
 }
--- src/test/java/ic/doc/WaccFrontendTest.java
+++ src/test/java/ic/doc/WaccFrontendTest.java
@@ -3,17 +3,13 @@ package ic.doc;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
 
 public class WaccFrontendTest {
 
     @Test
     public void frontendAcceptsCommandLineArguments() throws IOException {
-        Path path = Paths.get("src/test/resources/wacc_examples/invalid/syntaxErr/variables/badintAssignments.wacc");
-        String read = String.join("\n", Files.readAllLines(path));
-
-        System.out.println(WaccFrontend.parse(read));
+        InputStream inputStream = this.getClass().getResourceAsStream("/wacc_examples/valid/variables/intDeclaration.wacc");
+        System.out.println(WaccFrontend.parse(inputStream));
     }
 }
