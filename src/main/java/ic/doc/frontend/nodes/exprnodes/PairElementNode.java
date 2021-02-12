f3f3ce51405d6e6131f8550fbdbd17bacba1546f
Luke <lt1519@ic.ac.uk>
2021-02-12 18:29:30 +0800

    [lt1519] Added separate error message for accessing null pair

--- src/main/java/ic/doc/frontend/nodes/exprnodes/PairElementNode.java
+++ src/main/java/ic/doc/frontend/nodes/exprnodes/PairElementNode.java
@@ -1,5 +1,6 @@
 package ic.doc.frontend.nodes.exprnodes;
 
+import ic.doc.frontend.nodes.exprnodes.Literals.PairLiteralNode;
 import ic.doc.frontend.types.ErrorType;
 import ic.doc.frontend.types.PairType;
 import ic.doc.frontend.semantics.Visitor;
@@ -34,13 +35,19 @@ public class PairElementNode extends ExprNode {
 
   @Override
   public void check(Visitor visitor, ParserRuleContext ctx) {
-
+    /* Check if null pair */
+    if(expr instanceof PairLiteralNode){
+      visitor.getSemanticErrorList().addException(
+              ctx, "Cannot access element of null pair");
+      setType(new ErrorType());
+      return;
+    }
     /* Must be identifier with type pair. */
     if (!(expr instanceof VariableNode) || !(expr.getType() instanceof PairType)) {
       visitor.getSemanticErrorList().addException(
           ctx,
           pos.toString()
-              + " has to be called on identifier with type PAIR(T,_). Actual type: "
+              + " has to be called on identifier with type PAIR. Actual type: "
               + getExpr().getType().toString());
       setType(new ErrorType());
       return;
