abe8ff85a5264335e49fd42f52491dce483dbf01
Zhai Zirun <zz4319@ic.ac.uk>
2021-02-21 18:13:10 +0800

    [zz4319] implemented toAssembly for SDT and stack

--- src/main/java/ic/doc/backend/Instructions/SingleDataTransfer.java
+++ src/main/java/ic/doc/backend/Instructions/SingleDataTransfer.java
@@ -1,5 +1,19 @@
 package ic.doc.backend.Instructions;
 
+import ic.doc.frontend.types.*;
+
 public class SingleDataTransfer extends Instruction {
-    boolean loadFlag;
+    private boolean loadFlag;
+    private String cond = ""; //for conditional returns
+    private Type type = new AnyType();
+    private Operand dst;
+    private Operand expr;
+
+
+    @Override
+    public String toAssembly() {
+        String action = loadFlag ? "LDR " : "STR ";
+        String typeStr = loadFlag ? "" : type.toString();
+        return (action + cond + typeStr + dst.toString() + ", " + expr.toString());
+    }
 }
--- src/main/java/ic/doc/backend/Instructions/Stack.java
+++ src/main/java/ic/doc/backend/Instructions/Stack.java
@@ -3,4 +3,10 @@ package ic.doc.backend.Instructions;
 public class Stack extends Instruction{
   private boolean pushFlag;
   private int value;
+
+  @Override
+  public String toAssembly() {
+    String action = pushFlag ? "PUSH " : "POP ";
+    return (action + value);
+  }
 }
