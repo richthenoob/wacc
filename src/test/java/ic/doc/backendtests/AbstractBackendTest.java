693a067f4f0afb77eb811ddfed1914d1c89fdb7e
Jia Qi Poon <jqp18@ic.ac.uk>
2021-03-02 19:47:20 +0800

    [jqp18] Update backend tests to accept standard in

--- src/test/java/ic/doc/backendtests/AbstractBackendTest.java
+++ src/test/java/ic/doc/backendtests/AbstractBackendTest.java
@@ -16,13 +16,18 @@ import com.github.dockerjava.api.model.Frame;
 import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.Volume;
 import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.okhttp.OkDockerHttpClient;
 import ic.doc.backend.WaccBackend;
 import ic.doc.frontend.nodes.ProgNode;
 import ic.doc.frontendtests.AbstractFrontendTest;
 import java.io.File;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -106,10 +111,12 @@ public abstract class AbstractBackendTest {
    * any strings passed to stdout/stderr as a result of the emulation process.
    * Returns exit code. */
   private int emulateExecutable(String execFilePath,
-      StringBuilder stringOutput) {
+      StringBuilder stringOutput, String stdin) {
+
     /* Craft command object. */
     ExecCreateCmdResponse execCreateCmdResponse = dockerClient
         .execCreateCmd(CONTAINER_NAME)
+        .withAttachStdin(true)
         .withAttachStdout(true)
         .withAttachStderr(true)
         .withCmd("qemu-arm",
@@ -120,16 +127,28 @@ public abstract class AbstractBackendTest {
 
     /* Execute command on running docker container. */
     try {
-      dockerClient.execStartCmd(execCreateCmdResponse.getId())
+      /* Setup input stream to pass STDIN to container. */
+      PipedOutputStream out = new PipedOutputStream();
+      PipedInputStream in = new PipedInputStream(out);
+
+      Adapter<Frame> adapter = dockerClient
+          .execStartCmd(execCreateCmdResponse.getId())
+          .withStdIn(in)
           .exec(new Adapter<>() {
             @Override
             public void onNext(Frame frame) {
+              System.out.println(frame.toString());
               stringOutput.append(
                   new String(frame.getPayload(), StandardCharsets.UTF_8));
             }
-          })
-          .awaitCompletion();
-    } catch (InterruptedException e) {
+          });
+
+      /* Write to STDIN of container. */
+      out.write((stdin + "\n").getBytes(StandardCharsets.UTF_8));
+
+      adapter.awaitCompletion(5, TimeUnit.SECONDS);
+
+    } catch (Exception e) {
       System.err.println(e.getMessage());
     }
 
@@ -143,7 +162,9 @@ public abstract class AbstractBackendTest {
   /* Start a docker container if not already started from gradle build.
    * Also creates a temporary folder to output .s files to. */
   private void setupTest() {
-    dockerClient = DockerClientBuilder.getInstance().build();
+    dockerClient = DockerClientBuilder.getInstance()
+        .withDockerHttpClient(new OkDockerHttpClient.Builder().dockerHost(
+            URI.create("unix:///var/run/docker.sock")).build()).build();
 
     List<Container> containers = dockerClient.listContainersCmd()
         .withNameFilter(Collections.singleton(CONTAINER_NAME))
@@ -189,11 +210,11 @@ public abstract class AbstractBackendTest {
   }
 
   /* Compares exit code and output of emulation to reference compiler. */
-  private void checkEmulation(String filepath) {
+  private void checkEmulation(String filepath, String input) {
     /* Run generated executable through emulator. */
     StringBuilder outputMessages = new StringBuilder();
     int emulateExitCode = emulateExecutable("/compile/" + TEMP_EXEC_NAME,
-        outputMessages);
+        outputMessages, input);
     String emulateOutputMessage = outputMessages.toString()
         .replaceAll("\u0001|\u0002|\u0003|\u0004|\u0005|\u0006|\u0007", "");
 
@@ -226,6 +247,10 @@ public abstract class AbstractBackendTest {
   }
 
   public void backendTestFile(String testFilepath) {
+    backendTestFileWithInput(testFilepath, "");
+  }
+
+  public void backendTestFileWithInput(String testFilepath, String input) {
 
     /* Setup docker stuff and temporary folder if it doesn't exist. */
     setupTest();
@@ -241,13 +266,13 @@ public abstract class AbstractBackendTest {
 
     /* Generate code and write to temporary file. */
     String code = WaccBackend.generateCode(rootNode);
-    System.out.println(code);
+//    System.out.println(code);
     WaccBackend.writeToFile(TEMP_DIR_PATH + TEMP_ASSEMBLY_FILENAME, code);
 
     /* Cross compilation. */
     checkCrossCompilation();
 
     /* Emulation. */
-    checkEmulation(REFERENCE_DIR + testFilepath + "asm");
+    checkEmulation(REFERENCE_DIR + testFilepath + "asm", input);
   }
 }
