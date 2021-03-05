cbdaa4662b2bcfb8a25da0808dee6ba886569ea3
ry819 <ry819@ic.ac.uk>
2021-03-05 15:56:11 +0800

    [ry819] refactoring

--- src/main/java/ic/doc/frontend/nodes/exprnodes/Literals/IntLiteralNode.java
+++ src/main/java/ic/doc/frontend/nodes/exprnodes/Literals/IntLiteralNode.java
@@ -44,6 +44,7 @@ public class IntLiteralNode extends LiteralNode {
 
   @Override
   public void translate(Context context) {
+    /* Load integer value into register */
     ImmediateOperand operand = new ImmediateOperand<>(value.intValue()).withPrefixSymbol("=");
     RegisterOperand register = new RegisterOperand(context.getFreeRegister());
     context.addToCurrentLabel(SingleDataTransfer.LDR(register, operand));
