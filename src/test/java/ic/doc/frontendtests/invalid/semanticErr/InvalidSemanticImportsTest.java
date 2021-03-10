2c794f91c55ef9059e4e3e73c987fe5161ccee3a
Luke <lt1519@ic.ac.uk>
2021-03-10 15:27:04 +0800

    [lt1519] Added invalid semantic tests for imports

---
+++ src/test/java/ic/doc/frontendtests/invalid/semanticErr/InvalidSemanticImportsTest.java
@@ -0,0 +1,26 @@
+package ic.doc.frontendtests.invalid.semanticErr;
+
+import ic.doc.TestUtils;
+import ic.doc.frontendtests.AbstractFrontendTest;
+import java.util.Collection;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class InvalidSemanticImportsTest extends AbstractFrontendTest {
+
+  private static final String groupTestPath = "/invalid/semanticErr/imports/";
+
+  private static Collection<String> getTestNames() {
+    return TestUtils.getAllTestNames(groupTestPath);
+  }
+
+  @Tag("invalid")
+  @Tag("semantic")
+  @Tag("imports")
+  @ParameterizedTest
+  @MethodSource("getTestNames")
+  public void invalidSemanticIfTests(String testName) {
+    frontendTestFile(groupTestPath + testName);
+  }
+}
