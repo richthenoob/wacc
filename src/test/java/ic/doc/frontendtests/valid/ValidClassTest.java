e914ae50d51a626fb238ff26317ddcb52a99aafb
Zhai Zirun <zz4319@ic.ac.uk>
2021-03-10 18:13:58 +0800

    [zz4319] implemented lexing and parsing for classes, and two valid tests

---
+++ src/test/java/ic/doc/frontendtests/valid/ValidClassTest.java
@@ -0,0 +1,24 @@
+package ic.doc.frontendtests.valid;
+
+import ic.doc.TestUtils;
+import ic.doc.frontendtests.AbstractFrontendTest;
+import java.util.Collection;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class ValidClassTest extends AbstractFrontendTest {
+  private static final String groupTestPath = "/valid/classes/";
+
+  private static Collection<String> getTestNames() {
+    return TestUtils.getAllTestNames(groupTestPath);
+  }
+
+  @Tag("valid")
+  @Tag("class")
+  @ParameterizedTest
+  @MethodSource("getTestNames")
+  public void validSimpleFunctionTests(String testName) {
+    frontendTestFile(groupTestPath + testName);
+  }
+}
