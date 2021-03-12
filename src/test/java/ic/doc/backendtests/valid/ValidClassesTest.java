c4dae87c4ccc439588e440391e97aac232d8bc78
Jia Qi Poon <poonjiaqi@gmail.com>
2021-03-13 00:55:35 +0800

    [jqp18] Add backend tests for classes

---
+++ src/test/java/ic/doc/backendtests/valid/ValidClassesTest.java
@@ -0,0 +1,27 @@
+package ic.doc.backendtests.valid;
+
+import ic.doc.TestUtils;
+import ic.doc.backendtests.AbstractBackendTest;
+import java.util.Collection;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class ValidClassesTest extends AbstractBackendTest {
+
+  private static final String groupTestPath = "/valid/classes/";
+
+  private static Collection<String> getTestNames() {
+    return TestUtils.getAllTestNames(groupTestPath);
+  }
+
+  @Disabled
+  @Tag("backend")
+  @Tag("class")
+  @ParameterizedTest
+  @MethodSource("getTestNames")
+  public void validIfTests(String testName) {
+    backendTestFile(groupTestPath + testName);
+  }
+}
