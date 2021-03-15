facd59037cf0565cdd66db315f2536207c409c96
Zhai Zirun <zz4319@ic.ac.uk>
2021-03-16 05:18:52 +0800

    [zz4319] implemented semantic invalid tests

--- src/test/java/ic/doc/frontendtests/invalid/semanticErr/InvalidSemanticClassInheritanceTest.java
+++ src/test/java/ic/doc/frontendtests/invalid/semanticErr/InvalidSemanticClassInheritanceTest.java
@@ -16,7 +16,7 @@ public class InvalidSemanticClassInheritanceTest extends AbstractFrontendTest {
     return TestUtils.getAllTestNames(groupTestPath);
   }
 
-  @Disabled
+  //@Disabled
   @Tag("invalid")
   @Tag("semantic")
   @Tag("class")
